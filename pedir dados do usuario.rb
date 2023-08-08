47d88085f0ac32baa0b86bc7673bff73ffa51e15
CastionDev <jorgepocaderrimo.com@gmail.com>
2023-08-08 18:43:26 -0300

    primeiro codigo que fiz, serve para pedir dados do usuario

---
+++ pedir dados do usuario.rb
@@ -0,0 +1,7 @@
+print "digite seu nome: "
+nome = gets.chomp
+print "agora o seu sobrenome: "
+sobrenome = gets.chomp
+print "quantos anos voce tem #{nome}? "
+idade = gets.chomp
+print "olá #{nome} #{sobrenome}, você tem #{idade} anos, que legal!"
